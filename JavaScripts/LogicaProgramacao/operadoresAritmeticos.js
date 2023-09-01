334acc448bd46ed5db08f54e33a7657e870db4da
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-09-01 09:43:05 -0300

    Entendendo outros Operações Aritmética

---
+++ JavaScripts/LogicaProgramacao/operadoresAritmeticos.js
@@ -0,0 +1,8 @@
+const primeiroNumero = 5;
+const segundoNumero = 3;
+
+var media = (primeiroNumero + segundoNumero) / 2;
+var restoDivisao = (primeiroNumero % segundoNumero);
+var exponenciacao = (primeiroNumero ** segundoNumero);
+var adicionaMaisUm = primeiroNumero++;
+var diminuiMenosUm = primeiroNumero--;
