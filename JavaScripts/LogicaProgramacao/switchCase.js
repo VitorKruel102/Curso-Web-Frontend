257ecf6dbb34f50ce1b8b52b3e9d99aacd55796e
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-09-12 09:16:41 -0300

    Entendendo sobre switch case

---
+++ JavaScripts/LogicaProgramacao/switchCase.js
@@ -0,0 +1,15 @@
+let numero = 3;
+
+switch (numero) {
+    case 1:
+        alert('Número 1');
+        break;
+    case 2:
+        alert('Número 2');
+        break;
+    case 3:
+        alert('Número 3');
+        break;
+    default:
+        alert('Números de 1 a 3!');    
+}
