c76bfea722f54f51ac2087565a60d4830f42085f
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-12-19 11:11:38 -0300

    Conjunto de dados;

---
+++ JavaScripts/LogicaProgramacao/ConjuntoDeDados/scripts.js
@@ -0,0 +1,19 @@
+// Aula 546 - Arrays
+let product = new Array();
+product.push('Folders') // Adiciona dados
+
+let peoples = ['João', 'Maria', 'José'];
+peoples.pop()
+
+// Aula 547 - Iterar sobre arrays
+peoples_string = peoples.toString()
+
+for (let index = 0; index < peoples.length; index++) {
+    console.log(peoples[index]);
+}
+
+// Aula 552 - Objetos:
+let products = new Object();
+product.nome = 'Caneta';
+product.preco = 1.25;
+
