86c60e44ecf5fdbfade189f2d15833f23b977956
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2024-01-22 16:46:41 -0300

    Revisando as Variáveis em JS.

--- JavaScripts/LogicaProgramacao/tipos_dados.js
+++
@@ -1,8 +0,0 @@
-var numeroInteiro = 1;             // number
-var numeroDecimal = 1.1;           // number
-var string = 'String';             // string
-var boleanoVerdadeiro = true;      // booleano
-var boleanoFalso = false;          // booleano
-var lista = ['Item1', 'Item2'];    // array
-var vazio = null;                  // null
-var objeto = {};                   // object
